5540b0a8e0c7ff138b2d58017f0856e4d142efd7
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-03 17:48:15 -0600

    Using .env to get database

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,16 @@
 import { DataSource } from 'typeorm';
 
+const port: number = Number.isInteger(process.env['DATABASE_PORT'])
+  ? Number(process.env['DATABASE_PORT'])
+  : 5432;
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: '',
-  password: '',
-  database: 'videocreator',
+  port: port,
+  host: process.env['DATABASE_HOST'],
+  username: process.env['DATABASE_USER'],
+  password: process.env['DATABASE_PASS '],
+  database: process.env['DATABASE_NAME  '],
   logging: true,
   entities: ['./src/entity/*.ts'],
   subscribers: [],
