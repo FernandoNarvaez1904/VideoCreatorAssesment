e4ae4ba4b140373ff215a0ae3ce4455722a15920
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-04 17:18:27 -0600

    Creating route for getting the details of a single video using the ID

---
+++ src/controllers/video/getVideoDetails.controller.ts
@@ -0,0 +1,36 @@
+import { RequestHandler } from 'express';
+import { AppDataSource } from '../../data-source';
+import Video from '../../entity/Video';
+import { EntityNotFoundError } from 'typeorm';
+
+const getVideoDetailsController: RequestHandler = async (req, res) => {
+  try {
+    const videoRepository = AppDataSource.getRepository(Video);
+    // This is a workaround for a bug in which repository.findOneByOrFail
+    // returning object 4 when undefined is passed as id
+    const videoId = Number(req.params.id);
+
+    // Check if the videoId is not a number
+    if (isNaN(videoId)) {
+      return res
+        .status(400)
+        .json({ message: 'The provided id must be a number' });
+    }
+
+    // Getting video from id provided by user
+    const video = await videoRepository.findOneByOrFail({
+      id: videoId,
+    });
+    // Send a 201 Created response with the updated Video object as JSON
+    return res.status(201).json(video);
+  } catch (err) {
+    // If the error is an EntityNotFoundError (thrown when no entity is found with the specified ID)
+    if (err instanceof EntityNotFoundError) {
+      return res.status(400).json({ error: err.message });
+    }
+    // Giving error feedback to user
+    return res.status(500).json({ message: 'Error getting video' });
+  }
+};
+
+export default getVideoDetailsController;
--- src/routers/video.router.ts
+++ src/routers/video.router.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import getPublishedVideosController from '../controllers/video/getPublishedVideos.controller';
+import getVideoDetailsController from '../controllers/video/getVideoDetails.controller';
 import postCreateVideoController from '../controllers/video/postCreateVideo.controller';
 import postToggleIsPublishedVideoController from '../controllers/video/postToggleIsPublishedVideo.controller';
 
 const videoRouter = express.Router();
 
 videoRouter.get('/published', getPublishedVideosController);
+videoRouter.get('/detail/:id', getVideoDetailsController);
 
 videoRouter.post('/create', postCreateVideoController);
 videoRouter.post('/toggleIsPublished', postToggleIsPublishedVideoController);
