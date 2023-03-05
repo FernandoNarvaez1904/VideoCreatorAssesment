234d95bae5dc9d0c19e890fc41bf550426e4fc23
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-04 18:21:23 -0600

    Create route for Signing In using jwt

---
+++ src/controllers/user/postSignInUser.controller.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from 'express';
+import User from '../../entity/User';
+import { AppDataSource } from '../../data-source';
+import jwt from 'jsonwebtoken';
+
+const postSignInUserController: RequestHandler = async (req, res) => {
+  try {
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneByOrFail({
+      username: req.body.username,
+    });
+
+    // Checking if credentials are correct
+    const isCredentialsCorrect = await user.checkPassword(req.body.password);
+
+    // If credentials are incorrect, returning error response
+    if (!isCredentialsCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // If credentials are is correct, generating JWT token and sending it in response
+    const secretKey = process.env.SECRET_KEY as string;
+    const accessToken = jwt.sign({ id: user.id }, secretKey);
+    return res.status(201).json({ token: accessToken });
+  } catch (err) {
+    // Giving error feedback to user
+    return res.status(500).json({ message: 'Error Signing In User' });
+  }
+};
+
+export default postSignInUserController;
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import postSignUpUserController from '../controllers/user/postSignUpUser.controller';
+import postSignInUserController from '../controllers/user/postSignInUser.controller';
 
 const userRouter = express.Router();
 
-userRouter.post('/create', postSignUpUserController);
+userRouter.post('/signup', postSignUpUserController);
+userRouter.post('/signin', postSignInUserController);
 
 export default userRouter;
