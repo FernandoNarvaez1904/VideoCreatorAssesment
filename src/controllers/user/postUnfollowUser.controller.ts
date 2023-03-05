2b43e8cf94b70e1ab34ea8ad3e507f860ca376ce
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-05 10:23:22 -0600

    Creating endpoint for unfollowing users.

---
+++ src/controllers/user/postUnfollowUser.controller.ts
@@ -0,0 +1,52 @@
+import { RequestHandler } from 'express';
+import User from '../../entity/User';
+import { AppDataSource } from '../../data-source';
+import { Request as JWTRequest } from 'express-jwt';
+
+const postUnfollowUserController: RequestHandler = async (
+  req: JWTRequest,
+  res,
+) => {
+  try {
+    // Retrieving user repository from AppDataSource and finding user by id in the auth token
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneOrFail({
+      where: {
+        id: req.auth?.id,
+      },
+      relations: {
+        follows: true,
+        isFollowedBy: true,
+      },
+    });
+
+    const userId = Number(req.body.id);
+    // Check if the userId is not a number
+    if (isNaN(userId)) {
+      return res
+        .status(400)
+        .json({ message: 'The provided id must be a number' });
+    }
+    const otherUser = await userRepository.findOneOrFail({
+      where: {
+        id: userId,
+      },
+      // It is needed to query the relationships
+      relations: {
+        likedVideos: true,
+      },
+    });
+
+    // Following otherUser
+    user.follows = user.follows.filter((u) => u.id !== otherUser.id);
+    await userRepository.save(user);
+
+    // Returning success response with user and videoLiked object
+    return res.status(201).json(user);
+  } catch (err) {
+    // Giving error feedback to user
+    return res.status(500).json({ message: 'Error Following user' });
+  }
+};
+
+export default postUnfollowUserController;
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -10,6 +10,7 @@ import getUserLikedVideosController from '../controllers/user/getUserLikedVideos
 import getUserLikedVideosControllerWithId from '../controllers/user/getUserLikedVideosWithId.controller';
 import postDislikeVideoController from '../controllers/user/postDislikeVideo.controller';
 import postFollowUserController from '../controllers/user/postFollowUser.controller';
+import postUnfollowUserController from '../controllers/user/postUnfollowUser.controller';
 
 const userRouter = express.Router();
 
@@ -32,5 +33,6 @@ userRouter.post('/signin', postSignInUserController);
 userRouter.post('/likeVideo', postLikeVideoController);
 userRouter.post('/dislikeVideo', postDislikeVideoController);
 userRouter.post('/follow', postFollowUserController);
+userRouter.post('/unfollow', postUnfollowUserController);
 
 export default userRouter;
