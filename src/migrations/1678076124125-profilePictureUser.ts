ca3df8e8ad141e7652bcb96f7c365934ff832dd6
Diego Romero <diego.romero@unleashtechnologies.net>
2023-03-06 00:02:39 -0600

    Fix bug

--- src/migrations/1678076124125-profilePictureUser.ts
+++ src/migrations/1678076124125-profilePictureUser.ts
@@ -1,16 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class profilePictureUser1678076124125 implements MigrationInterface {
-    name = 'profilePictureUser1678076124125'
+  name = 'profilePictureUser1678076124125';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "video" DROP COLUMN "profilePictureUrl"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "profilePictureUrl" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "profilePictureUrl" character varying`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "profilePictureUrl"`);
-        await queryRunner.query(`ALTER TABLE "video" ADD "profilePictureUrl" character varying`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" DROP COLUMN "profilePictureUrl"`,
+    );
+  }
 }
